3340462a3dab6d0f5f05a085fcadd1d87cbd0427
JuMoth <133859287+JuMoth@users.noreply.github.com>
2024-02-19 19:08:22 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,24 @@
+function solution(A, B) {
+    if (A.length !== B.length) {
+        return -1;
+    }
+    
+    // const doubledA = A + A;
+    // return doubledA.indexOf(B);
+    
+    let count = 0;
+    while (A !== B) {
+        A = A[A.length-1] + A.slice(0, A.length-1);
+        count++;
+
+        if (A === B) {
+            return count;
+        }
+        
+        if (count > A.length) {
+            return -1;
+        }
+    }
+
+    return count;
+}
