1cbe0535ddfe4cc783acb84d5fc74694a12c31b4
JuMoth <133859287+JuMoth@users.noreply.github.com>
2024-02-23 16:24:47 +0900

    [level 2] Title: 구명보트, Time: 11.38 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,18 @@
+function solution(people, limit) {
+    people.sort((a, b) => b - a);
+    let left = 0;
+    let right = people.length - 1;
+    let answer = 0;
+
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+            right--;
+        } else {
+            left++;
+        }
+        answer++;
+    }
+
+    return answer;
+}
