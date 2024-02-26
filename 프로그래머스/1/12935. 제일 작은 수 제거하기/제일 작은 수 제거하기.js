af6be9b7545f60885cf711777461a331ea020df4
JuMoth <133859287+JuMoth@users.noreply.github.com>
2024-02-26 18:52:15 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    if (arr.length <= 1) return [-1];
+
+    const min = Math.min(...arr);
+    const filteredArr = arr.filter(num => num !== min);
+
+    return filteredArr.length > 0 ? filteredArr : [-1];
+}
