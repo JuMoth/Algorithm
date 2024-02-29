b6c84c82c12995e1a3be73ca37832d1eed8fbbe4
JuMoth <133859287+JuMoth@users.noreply.github.com>
2024-02-29 13:42:52 +0900

    [Bronze V] Title: A+B - 8, Time: 120 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let cnt = Number(input[0]);
+let ans = '';
+
+for(let i=1; i<=cnt; i++) {
+    let ab = input[i].split(' ');
+    ans += `Case #${i}: ${Number(ab[0])} + ${Number(ab[1])} = ${Number(ab[0])+Number(ab[1])}` + "\n";
+}
+
+console.log(ans);
