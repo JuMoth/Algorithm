7012cb2417618ccf109173a06b93e28cd3d6c713
JuMoth <133859287+JuMoth@users.noreply.github.com>
2024-03-04 18:36:34 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString();
+const dial = {
+  ABC: 3, DEF: 4, GHI: 5, JKL: 6, MNO: 7, PQRS: 8, TUV: 9, WXYZ: 10,
+};
+
+let minTime = 0;
+for(let i=0; i<input.length; i++) {
+    for(let j in dial) {
+        if(j.includes(input[i])) {
+            minTime += dial[j];
+        }
+    }
+}
+
+console.log(minTime);
