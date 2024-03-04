e0b0021538d4f3d96cbb58c5fe3dc8f4b923bb9a
JuMoth <133859287+JuMoth@users.noreply.github.com>
2024-03-04 17:29:07 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        long A = s.nextLong();
+        long B = s.nextLong();
+        long C = s.nextLong();
+        
+        s.close();
+        
+        System.out.println(A+B+C);
+    }
+}
