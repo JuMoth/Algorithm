42be40cf010e21bfe72fb93935ccdb9408ba513f
JuMoth <tkdlqjqkd@gmail.com>
2024-03-22 11:44:53 +0900

    [Bronze II] Title: 수 정렬하기, Time: 136 ms, Memory: 9764 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,8 @@
+let [N, ...nums] = require('fs')
+    .readFileSync(process.platform === "linux"?'/dev/stdin':'./input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(Number);
+
+console.log(nums.sort((a,b)=>a-b).join('\n'));
