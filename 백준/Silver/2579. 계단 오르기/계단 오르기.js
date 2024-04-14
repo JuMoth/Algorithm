092079d3dc417b43703ec8a0db3f489d0f494bc1
JuMoth <tkdlqjqkd@gmail.com>
2024-04-14 09:19:31 +0900

    [Silver III] Title: 계단 오르기, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -8,7 +8,7 @@ const input = require('fs')
 const stepCount = input[0];
 // index를 1부터 시작하기 위해
 const dp = new Array(stepCount+1);
-
+// 메모이제이션
 dp[1] = input[1]; // 계단이 1개인 경우
 dp[2] = dp[1] + input[2]; // 계단이 2개인 경우
 dp[3] = Math.max(input[1], input[2]) + input[3]; // 계단이 3개인 경우
