9c2ea288596c1869acd2c075232a1da5f31a4a8e
JuMoth <tkdlqjqkd@gmail.com>
2024-10-05 14:29:08 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,3 +1,5 @@
 function solution(my_string, letter) {
-    return my_string.split('').filter(v => v!==letter).join('');
+    let answer = '';
+    answer = my_string.split('').filter(v => v!==letter).join('');
+    return answer;
 }
