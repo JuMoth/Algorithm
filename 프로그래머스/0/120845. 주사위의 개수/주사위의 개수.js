c69c9150942862f6c436cca631749883f4a5c0e7
JuMoth <tkdlqjqkd@gmail.com>
2024-10-09 12:04:31 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,7 @@
 function solution(box, n) {
     var answer = 1;
     for(let num of box) {
-        answer = answer * Math.floor(num/n);
+        answer = answer*Math.floor(num/n);
     }
     return answer;
 }
