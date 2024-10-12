391ce7b4c2892ee45b47791fb1b5bbc9229d4513
JuMoth <tkdlqjqkd@gmail.com>
2024-10-13 00:40:48 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,8 +1,7 @@
 function solution(sides) {
     var answer = 0;
-    let numArr = sides.sort((a, b) => a-b);
-    console.log(numArr);
-    if(numArr[2]< numArr[0]+numArr[1]) {
+    let numArr = sides.sort((a, b) => a - b);
+    if(numArr[2]< numArr[0] + numArr[1]) {
         answer = 1;
     } else {
         answer = 2;
