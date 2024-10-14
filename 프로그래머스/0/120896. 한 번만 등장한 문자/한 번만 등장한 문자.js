a3b357d4ee889073d3a3d17aafd5f3b13c11d6e7
JuMoth <tkdlqjqkd@gmail.com>
2024-10-14 14:34:33 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 5.75 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,7 +1,6 @@
 function solution(s) {
     let res = [];
     for(let c of s) console.log(s.indexOf(c));
-    console.log("----------------");
     for(let c of s) console.log(s.lastIndexOf(c));
     for (let c of s) if (s.indexOf(c) === s.lastIndexOf(c)) res.push(c);
     return res.sort().join('');
