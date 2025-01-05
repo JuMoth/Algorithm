6f236d1061bc62134fd7439383aec864f76ad6b7
JuMoth <tkdlqjqkd@gmail.com>
2025-01-05 17:19:55 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.19 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -4,12 +4,12 @@ function solution(n) {
     while(val !== 1) {
         answer.push(val)
         if(val%2 === 0) {
-            val/=2
+            val/=2;
         } else {
-            val = 3 * val + 1
+            val = 3 * val + 1;
         }
     }
-    answer.push(1)
+    answer.push(1);
 
-    return answer
+    return answer;
 }
