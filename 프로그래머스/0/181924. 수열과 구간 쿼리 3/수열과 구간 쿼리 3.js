ada0b084cc0247e9a46e278eaa880ad20a617094
JuMoth <tkdlqjqkd@gmail.com>
2025-01-09 20:19:36 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.45 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    let answer = [...arr];
+    for(let i=0; i<queries.length; i++){
+        let box = answer[queries[i][0]]
+        answer[queries[i][0]]=answer[queries[i][1]];
+        answer[queries[i][1]]=box 
+    }
+    return answer;
+}
