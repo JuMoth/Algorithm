9f315ab6d9d4d960d6d2b29992419cb21fc4866a
JuMoth <tkdlqjqkd@gmail.com>
2025-02-10 20:02:13 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,7 +1,7 @@
 function solution(str1, str2) {
     let answer = '';
-    const arra = str1.split('');
-    const arrb = str2.split('');
+    let arra = str1.split('');
+    let arrb = str2.split('');
     for(let i=0; i<arra.length; i++) {
         answer += arra[i] + arrb[i];
     }
