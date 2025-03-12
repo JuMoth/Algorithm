3a802300cfb2ac45578a43730efd4c620ce7830b
JuMoth <tkdlqjqkd@gmail.com>
2025-03-12 19:58:59 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
-    var answer = '';
+    let answer = '';
     let strArr = my_string.split("");
     for(let i=0; i<strArr.length; i++) {
         if(!answer.includes(strArr[i])) {
