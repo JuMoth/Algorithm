8b2b4711b0ea5e17965da7c9c51487c8aab72070
JuMoth <tkdlqjqkd@gmail.com>
2026-07-04 18:57:04 +0900

    [level 1] Title: 추억 점수, Time: 0.42 ms, Memory: 44.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,11 @@
+function solution(name, yearning, photo) {
+    const score = {};
+
+    for (let i=0; i<name.length; i++) {
+        score[name[i]] = yearning[i];
+    }
+
+    return photo.map(people =>
+        people.reduce((sum, person) => sum + (score[person] || 0), 0)
+    );
+}
