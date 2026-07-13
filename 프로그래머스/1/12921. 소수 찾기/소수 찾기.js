e343053c28b9d090c6e6d46959167bfcec4210b0
JuMoth <tkdlqjqkd@gmail.com>
2026-07-13 10:58:34 +0900

    [level 1] Title: 소수 찾기, Time: 137.39 ms, Memory: 48.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let result = 0;
+    
+    for(let i=2; i<=n; i++) {
+        let isPrime = true;
+        
+        for(let j=2; j*j<=i; j++) {
+            if(i%j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime) result++;
+    }
+    return result;
+}
