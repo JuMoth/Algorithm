c4a8993ed0f541e673f27b84bd38ac9a54f15698
JuMoth <tkdlqjqkd@gmail.com>
2026-08-12 20:51:35 +0900

    [level 2] Title: 점 찍기, Time: 11.13 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.js
@@ -0,0 +1,9 @@
+function solution(k, d) {
+    let answer = 0;
+    
+    for(let x=0; x<=d; x+=k) {
+        let maxY = Math.sqrt(d*d - x*x);
+        answer += Math.floor(maxY/k)+1;
+    }
+    return answer;
+}
