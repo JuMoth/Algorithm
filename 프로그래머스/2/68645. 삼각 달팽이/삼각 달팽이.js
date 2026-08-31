b4ca84ab8b80c6098c7fd5fec4ad11aecd868fb9
JuMoth <tkdlqjqkd@gmail.com>
2026-08-31 18:15:46 +0900

    [level 2] Title: 삼각 달팽이, Time: 21.67 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    let arr = Array.from({ length: n }, (_, i) => 
+        Array(i+1).fill(0)
+    );
+    
+    let num = 1;
+    let x = -1;
+    let y = 0;
+    
+    for(let i=0; i<n; i++) {
+        for(let j=i; j<n; j++) {
+            if(i%3 === 0) x++;
+            else if(i%3 === 1) y++;
+            else {
+                x--;
+                y--;
+            }
+            arr[x][y] = num++;
+        }
+    }
+    return arr.flat();
+}
