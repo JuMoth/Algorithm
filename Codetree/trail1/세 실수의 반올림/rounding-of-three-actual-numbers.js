e9e242ef43c6596bce4e97c8a2a6610273aa8b2a
JuMoth <tkdlqjqkd@gmail.com>
2026-09-19 22:07:10 +0900

    [Passed] 세 실수의 반올림 (63ms, 10MB)

---
+++ Codetree/trail1/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+let [a, b, c] = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+console.log(a.toFixed(3));
+console.log(b.toFixed(3));
+console.log(c.toFixed(3));
